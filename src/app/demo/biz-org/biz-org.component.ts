ca47946965b82d15dff42051723cae151f8df033
litaojian <litaojian88@gmail.com>
2018-03-19 17:20:15 +0800

    ok

--- src/app/demo/biz-org/biz-org.component.ts
+++ src/app/demo/biz-org/biz-org.component.ts
@@ -6,45 +6,46 @@ import { BizTreeTableComponent } from '@yg-widget/biz-app';
 
 
 @Component({
-    selector: 'biz-org',
-    templateUrl: './biz-org.component.html',
-    styleUrls:[]
+  selector: 'biz-org',
+  templateUrl: './biz-org.component.html',
+  styleUrls: []
 })
 export class BizOrganizationComponent extends BizTreeTableComponent implements OnInit, OnDestroy {
-    
-    constructor(injector: Injector, private service: TestRecService) {
-        super(injector);
-        this.bizService = service;
-        console.log("BizOrganizationComponent init .........");
-        this._treeNodes = [
-            {
-              id: 1,
-              name: '系统菜单树',
-              children: [
-                {
-                  id: 2,
-                  name: 'child1'
-                }, {
-                  id: 3,
-                  name: 'child2'
-                }
-              ]
-            },{
-                id: 4,
-                name: 'root2'                  
-            }
-          ];
-    }
+  _treeNodes:any;
 
-    ngOnInit() {
-		super.ngOnInit();
-		//this.setPageSize(10);
-    }
-    
+  constructor(injector: Injector, private service: TestRecService) {
+    super(injector);
+    this.bizService = service;
+    console.log("BizOrganizationComponent init .........");
+    this._treeNodes = [
+      {
+        id: 1,
+        name: '系统菜单树',
+        children: [
+          {
+            id: 2,
+            name: 'child1'
+          }, {
+            id: 3,
+            name: 'child2'
+          }
+        ]
+      }, {
+        id: 4,
+        name: 'root2'
+      }
+    ];
+  }
 
-	ngOnDestroy(){	
-		//console.log(" testRec ngOnDestory......");				
-    }
-    
+  ngOnInit() {
+    super.ngOnInit();
+    //this.setPageSize(10);
+  }
+
+
+  ngOnDestroy() {
+    //console.log(" testRec ngOnDestory......");				
+  }
+
 
 }
